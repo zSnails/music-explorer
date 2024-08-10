0a35b0eb76150719a19011e647627f80aab59a62
zSnails <erizojuan33@gmail.com>
2024-08-10 11:44:57 -0600

    feat: add pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,12 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
-    modules: ["@nuxt/ui"],
+    modules: ["@nuxt/ui", "@pinia/nuxt"],
 
     runtimeConfig: {
-        // Make environment variables accessible here
         public: {
             CLIENT_ID: process.env.CLIENT_ID,
             CALLBACK_URL: process.env.CALLBACK_URL,
-            // Add more environment variables as needed
         }
     }
 });
