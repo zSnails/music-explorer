c8b0f367c363a9ce59469b5247532ec5fd7976b7
zSnails <erizojuan33@gmail.com>
2024-08-10 11:44:45 -0600

    feat: setup main layout

--- app.vue
+++ app.vue
@@ -1,7 +1,28 @@
 <template>
-    <div>
-        <NuxtPage></NuxtPage>
+    <div class="grid gap-8 min-h-[100dvh] grid-rows-[auto_1fr_auto]">
+        <header>
+            <nav class="flex py-3 px-5 flex-row w-full h-fit justify-between bg-slate-100 shadow">
+                <div>
+                    <ULink class="text-xl" to="/">Music Explorer</ULink>
+                </div>
+                <div class="flex flex-row">
+                    <ULink to="/music">Music</ULink>
+                </div>
+                <div>
+                    <UButton v-if="!isLogged" to="/login">Login</UButton>
+                </div>
+            </nav>
+        </header>
+        <main class="mx-auto">
+            <NuxtPage />
+        </main>
+        <footer>
+            <h1>Footer</h1>
+        </footer>
     </div>
 </template>
 <script setup lang="ts">
+import { useStorage } from "@vueuse/core";
+const token = useStorage("access_token", "");
+const isLogged = computed(() => token.value !== "");
 </script>
