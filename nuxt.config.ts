0d9d0a6671049df4d4ff1589a90c4da2c6753230
zSnails <erizojuan33@gmail.com>
2024-08-10 16:15:59 -0600

    fix: remove pinia dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
-    modules: ["@nuxt/ui", "@pinia/nuxt"],
+    modules: ["@nuxt/ui"],
 
     runtimeConfig: {
         public: {
