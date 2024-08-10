eadcdfc655e5e73b0f54e09d3a32c20ce9a34e4b
zSnails <erizojuan33@gmail.com>
2024-08-09 20:47:46 -0600

    fix: expose some env variables and add the nuxt-ui module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    modules: ["@nuxt/ui"],
+
+    runtimeConfig: {
+        // Make environment variables accessible here
+        public: {
+            CLIENT_ID: process.env.CLIENT_ID,
+            CALLBACK_URL: process.env.CALLBACK_URL,
+            // Add more environment variables as needed
+        }
+    }
+});
