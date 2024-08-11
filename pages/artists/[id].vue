54d355222f574f2715e7d85598c7e3e001cfbe3a
noni <noni45100@hotmail.com>
2024-08-11 12:53:48 -0600

    replace table to ucards

--- pages/artists/[id].vue
+++ pages/artists/[id].vue
@@ -3,7 +3,7 @@
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { useStorage, useDebounceFn } from '@vueuse/core';
-
+import { format } from 'date-fns';
 const token = useStorage("access_token", "");
 const route = useRoute();
 
@@ -117,7 +117,8 @@ const albumsColumns = [
 ];
 
 
-
+const linkdemirda = "https://www.youtube.com/";
+const imagendemierda = "https://preview.redd.it/fdog4gcr20551.png?width=572&format=png&auto=webp&s=e961bddb8c12d5be17ecd388bd5e4a61e1bd3c20"
 
 const albumsTest = [
     {
@@ -181,11 +182,16 @@ interface Album {
     release_date: string;
     images: Image[];
     total_tracks: number;
+    external_urls: ExternalUrls;
 };
 
 interface Followers {
     href: null;
     total: number;
+};
+
+interface ExternalUrls {
+    spotify: string;
 };
 
 
@@ -207,16 +213,18 @@ const loadTracks = async (): Promise<Tracks> => {
             duration_ms: response.tracks[i].duration_ms,
             explicit: response.tracks[i].explicit,
             release_date: response.tracks[i].album.release_date,
-            images: response.tracks[i].album.images
+            images: response.tracks[i].album.images,
         }
         if (song.explicit) {
-            song.explicit = "E";
+            song.explicit = "Explicit";
         } else {
             song.explicit = "";
         }
 
-        song.duration_ms = Number((song.duration_ms / 60000).toFixed(2));
+       
         songs.value.push(song);
+
+        
 
         const image = songs.value[i].images[0].url;
         images.value.push(image);
@@ -238,7 +246,7 @@ const loadArtist = async () => {
 
     artist.value = response.name;
     followers.value = response.followers.total;
-    images.value = [response.images[0].url];
+    //images.value = [response.images[0].url];
 
 }
 
@@ -259,7 +267,8 @@ const loadAlbums = async () => {
             name: response.items[i].name,
             release_date: response.items[i].release_date,
             images: response.items[i].images,
-            total_tracks: response.items[i].total_tracks
+            total_tracks: response.items[i].total_tracks,
+            external_urls: response.items[i].external_urls,
         }
         console.log(album.total_tracks);
         if (album.total_tracks > 1) {
@@ -269,7 +278,6 @@ const loadAlbums = async () => {
 
     console.log(albums.value);
 
-    //return albums.value = response.tracks;
 }
 
 
@@ -297,7 +305,8 @@ onMounted(() => {
         <div class="flex justify-between ">
             <div>
                 <h1 class="text-xl font-bold mb-4">Artist: {{ artist }}</h1>
-                <h1 class="text-xl font-bold font-sans">Followers: {{ new Intl.NumberFormat("en-US", { notation: "compact" }).format(followers) }}</h1>
+                <h1 class="text-xl font-bold font-sans">Followers: {{ new Intl.NumberFormat("en-US", { notation:
+                    "compact" }).format(followers) }}</h1>
             </div>
             <div>
                 <UCarousel ref="carouselRef" v-slot="{ item }" :items="images"
@@ -310,11 +319,41 @@ onMounted(() => {
     <div class="flex justify-between items-start ">
         <div class="pr-8">
             <h1 class="text-xl font-bold mb-8">Top Tracks</h1>
-            <UTable :columns="tracksColumns" :rows="songs" :ui="tableConfig" />
+            <ClientOnly fallback-tag="span" fallback="Loading songs...">
+                <UCard v-for="(song, idx) in songs" :key="idx" as="a" target="_blank" :href="songs[idx].album.external_urls.spotify"
+                    class="shadow-xl hover:scale-105 hover:bg-slate-100 hover:animate-pulse min-w-[500px] max-w-[500px] h-fit max-h-[200px] w-full flex flex-col gap-6">
+                    <div class="flex flex-row justify-between">
+                        <img :src="song.images[0].url" :alt="song.name" width="100" class="mb-2 rounded-xl">
+                        <h1 class="font-bold text-xl">{{ song.name }}</h1>
+                        <h1 class="font-bold text-lg">{{ format(song.duration_ms, 'mm:ss') }}</h1>
+                        <h1 class="font-bold text-green-500">{{ song.explicit }}</h1>
+                    </div>
+                    <div class="flex flex-row justify-between items-baseline truncate text-ellipsis">
+                        <div class="flex flex-col">
+                        </div>
+                        <UIcon class="w-8 h-8" name="i-heroicons-arrow-top-right-on-square-20-solid"></UIcon>
+                    </div>
+                </UCard>
+            </ClientOnly>
         </div>
         <div class=" pl-2">
             <h1 class="text-xl font-bold mb-8">Albums</h1>
-            <UTable :columns="albumsColumns" :rows="albums" :ui="tableConfig" />
+            <ClientOnly fallback-tag="span" fallback="Loading songs...">
+                <UCard v-for="(album, idx) in albums" :key="idx" as="a" target="_blank" :href="album.external_urls.spotify"
+                    class="shadow-xl hover:scale-105 hover:bg-slate-100 hover:animate-pulse min-w-[500px] max-w-[500px] h-fit max-h-[200px] w-full flex flex-col gap-6">
+                    <div class="flex flex-row justify-between">
+                        <img :src="album.images[0].url" :alt="album.name" width="100" class="mb-2 rounded-xl">
+                        <h1 class="font-bold text-4xl">{{ album.name }}</h1>
+                        <h1 class="font-bold text-blue-500">tracks: {{ album.total_tracks }}</h1>
+                    </div>
+                    <div class="flex flex-row justify-between items-baseline truncate text-ellipsis">
+                        <div class="flex flex-col">
+                            <h1 class="font-bold text-2xl">{{ album.release_date }}</h1>
+                        </div>
+                        <UIcon class="w-8 h-8" name="i-heroicons-arrow-top-right-on-square-20-solid"></UIcon>
+                    </div>
+                </UCard>
+            </ClientOnly>
         </div>
     </div>
 </template>
