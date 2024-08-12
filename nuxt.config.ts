c2330903d2f8c454401e774f5d22bc71cb1558c0
noniLaptop <noni45100@hotmail.com>
2024-08-12 11:10:48 -0600

    the page use light mode by default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,15 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
     modules: ["@nuxt/ui"],
+    
+    colorMode: {
+        preference: 'light' // disable dark mode
+    },
 
     runtimeConfig: {
         public: {
             CLIENT_ID: process.env.CLIENT_ID,
             CALLBACK_URL: process.env.CALLBACK_URL,
         }
-    }
+    },
 });
